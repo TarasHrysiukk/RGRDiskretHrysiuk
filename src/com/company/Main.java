a71298fd2c14c824b367fdc569a2559e080c3a0f
TarasHrysiukk <t.grusyk@gmail.com>
2021-04-04 18:15:43 +0300

    пошуку компонент зв’язності у графі.

---
+++ src/com/company/Main.java
@@ -0,0 +1,83 @@
+package com.company;
+
+import java.util.*;
+
+class ComponentsBound
+{
+    private static int[][] G;
+    private static int N;
+
+    private static boolean[] visited;
+
+    // обхід графа в глибину з заданної вершини vertex
+    private static void dfs(int vertex)
+    {
+        System.out.print(vertex + " ");
+
+        visited[vertex] = true;
+
+        for (int i = 0; i < N; i++)
+        {
+            if (!visited[i] && G[vertex][i] == 1) // якщо i-та вершина не пройдена и є шлях vertex -> i
+            {
+                dfs(i);
+            }
+        }
+    }
+
+    // рошук компонент і вивід їх  на екран
+    private static void findComponents()
+    {
+        visited = new boolean[ N ]; //масив пройденних вершин, true - значить пройдена, false ні
+        Arrays.fill(visited, false);
+
+        System.out.println("Components: ");
+
+        for (int i = 0; i < N; i++)
+        {
+            if (!visited[i])
+            {
+                System.out.print("[ ");
+
+                dfs(i);
+
+                System.out.println("]");
+            }
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Random r = new Random();
+
+        N = 10;
+
+        G = new int[ N ][ N ]; // матриця суміжності( G - Graph )
+
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                if ( r.nextInt(N) == j ) {
+                    G[i][j] = 1;
+                }
+                else {
+                    G[i][j] = 0;
+                }
+            }
+        }
+
+        //результат
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                System.out.print(G[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+
+        findComponents();
+    }
+}
